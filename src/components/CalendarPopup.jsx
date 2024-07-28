9a903eddc72789f8513f5f581c658a2ca264421f
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-07-28 19:15:41 +0900

    feat: 달력 다이얼로그 구현 - 커스텀 달력 구현 - 달력 다이얼로그 구현

---
+++ src/components/CalendarPopup.jsx
@@ -0,0 +1,35 @@
+import Calendar from 'react-calendar';
+import PropTypes from 'prop-types';
+import '../styles/Calendar.css';
+import './CalendarPopup.css';
+import moment from 'moment';
+
+const CalendarPopup = ({ isVisible, onClose , selectedDate, onDateChange, onConfirm}) => {
+    if (!isVisible) return null;
+
+    return (
+        <div className="calendar-popup">
+            <Calendar
+                value={selectedDate}
+                onClickDay={onDateChange}
+                showNeighboringMonth={false}
+                formatDay={(locale, date) => moment(date).format("DD")}
+                locale="en-GB" // 영어 달력이 더 이뻐보여서 설정했어요. 한글로 설정하려면 지우시면 됩니당
+            />
+            <div className="buttons" >
+                <button className="close-btn" onClick={onClose}>취소</button>
+                <button className="confirm-btn" onClick={onConfirm} disabled={!selectedDate}>선택</button>
+            </div>
+        </div>
+    );
+};
+
+CalendarPopup.propTypes = {
+    isVisible: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired,
+    selectedDate: PropTypes.instanceOf(Date),
+    onDateChange: PropTypes.func.isRequired,
+    onConfirm: PropTypes.func.isRequired,
+};
+
+export default CalendarPopup;
