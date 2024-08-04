7337a586783e58377be8e272541cfb288b8b073d
charmeee <mandugohigh@gmail.com>
2024-08-04 23:39:09 +0900

    feat: util에 emotion에따른 이미지 src받는거추가

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,63 +1,79 @@
+import happySvg from "../assets/happy.svg";
+import angrySvg from "../assets/angry.svg";
+import embarSvg from "../assets/embar.svg";
+import neutralSvg from "../assets/soso.svg";
+import anxiousSvg from "../assets/anxious.svg";
+import excitedSvg from "../assets/joy.svg";
+import sadSvg from "../assets/sad.svg";
+import tiredSvg from "../assets/strength.svg";
+
 export const getKoDayOfWeek = (dayOfWeek) => {
-    switch (dayOfWeek) {
-        case 0:
-            return "일요일";
-        case 1:
-            return "월요일";
-        case 2:
-            return "화요일";
-        case 3:
-            return "수요일";
-        case 4:
-            return "목요일";
-        case 5:
-            return "금요일";
-        case 6:
-            return "토요일";
-    }
+  switch (dayOfWeek) {
+    case 0:
+      return "일요일";
+    case 1:
+      return "월요일";
+    case 2:
+      return "화요일";
+    case 3:
+      return "수요일";
+    case 4:
+      return "목요일";
+    case 5:
+      return "금요일";
+    case 6:
+      return "토요일";
+  }
 }
 
 export const getUsDayOfWeek = (dayOfWeek) => {
-    switch (dayOfWeek) {
-        case 0:
-            return "Sun";
-        case 1:
-            return "Mon";
-        case 2:
-            return "Tue";
-        case 3:
-            return "Wed";
-        case 4:
-            return "Thu";
-        case 5:
-            return "Fri";
-        case 6:
-            return "Sat";
-    }
+  switch (dayOfWeek) {
+    case 0:
+      return "Sun";
+    case 1:
+      return "Mon";
+    case 2:
+      return "Tue";
+    case 3:
+      return "Wed";
+    case 4:
+      return "Thu";
+    case 5:
+      return "Fri";
+    case 6:
+      return "Sat";
+  }
 }
 
 export const getImagePathByEmotion = (emotion) => {
-    const imagePaths = {
-        불안: "../src/assets/anxious.svg",
-        보통: "../src/assets/soso.svg",
-        힘듦: "../src/assets/strength.svg",
-        즐거움: "../src/assets/joy.svg",
-        행복: "../src/assets/happy.svg",
-        화남: "../src/assets/angry.svg",
-        당황스러움: "../src/assets/embar.svg",
-        슬픔: "../src/assets/sad.svg"
-    };
-
-    return imagePaths[emotion] || "../src/assets/error_icn.svg"; // 키워드가 없으면 null 반환
+  switch (emotion) {
+    case "ANGRY":
+      return angrySvg;
+    case "HAPPY":
+      return happySvg;
+    case "EMBARRASSED":
+      return embarSvg;
+    case "SAD":
+      return sadSvg;
+    case "NEUTRAL":
+      return neutralSvg;
+    case "EXCITED":
+      return excitedSvg;
+    case "ANXIOUS":
+      return anxiousSvg;
+    case "TIRED":
+      return tiredSvg;
+    default:
+      return "";
+  }
 };
 
 
-export const setAuthStorage = ({token, vender}) => {
-    localStorage.setItem('token', token);
-    localStorage.setItem('vender', vender);
+export const getDateString = (date) => {
+  return date.getFullYear() + "-" + (date.getMonth() + 1).toString().padStart(2, "0") + "-" + date.getDate().toString().padStart(2, "0");
 }
 
-export const resetAuthStorage = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('vender');
+export const convertToDate = (dateString) => {
+  const [year, month, day] = dateString.split("-");
+  return new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
 }
