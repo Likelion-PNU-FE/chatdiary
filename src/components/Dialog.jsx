7ad2d9019312bf814effca4ee007f273d193afb4
charmeee <mandugohigh@gmail.com>
2024-08-04 19:36:22 +0900

    feat: 공통 dialog컴포넌트생성

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,10 +1,10 @@
 import './Dialog.scss';
 
-const Dialog = ({isOpen, setOpen, width, children}) => {
+const Dialog = ({isOpen, setOpen, style, children}) => {
   return (
     <div className={`dialog ${isOpen ? 'open' : ''}`}>
-      <div className="content" style={{width: width ? width : 'auto'}}>
-        <button className="close" onClick={() => setOpen(false)}>X</button>
+      <div className="content" style={style}>
+        <button className="close" onClick={() => setOpen(false)}>ㄨ</button>
         {children}
       </div>
     </div>
